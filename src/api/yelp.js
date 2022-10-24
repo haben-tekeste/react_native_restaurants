bd92222c55ff33339ad76cf9df90a2b2bf58b670
haben-tekeste <haben.t00@gmail.com>
2022-10-24 19:54:45 +0400

    updated api

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses/",
   headers: {
-    Authorization:
-      "Bearer Loz9DVbZpmc5V8BDMiUFoIS8r85oUF2OmjWaI_fUWI7AOXO1HwjIhu6OMU_a0KJ0IU820DKUOgA9VEI3Iqy948dinZXdFZ0JUQs2rMp7qHG9s05G9-IqAsVzWyMTY3Yx",
+    Authorization: `Bearer ${process.env.REACT_APP_YELP_KEY}`,
   },
 });
